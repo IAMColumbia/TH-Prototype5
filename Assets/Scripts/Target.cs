2159ae489ab7bd989adbb7b2d46757e5f4c87e6a
Fulgore2002 <typryorhitchcock1@gmail.com>
2024-11-05 21:11:02 -0600

    Replaced messy code with new methods

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,23 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Target : MonoBehaviour
 {
+    private float _minSpeed = 12; 
+    private float _maxSpeed = 16; 
+    private float _maxTorque = 10; 
+    private float _xRange = 4; 
+    private float _ySpawnPos = -6;
     private Rigidbody _targetRb;
 
     // Start is called before the first frame update
     void Start()
     {
         _targetRb = GetComponent<Rigidbody>();
-        _targetRb.AddForce(Vector3.up* Random.Range(12, 16), ForceMode.Impulse);
-        _targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
-        transform.position = new Vector3(Random.Range(-4, 4), -6);
+        _targetRb.AddForce(RandomForce(), ForceMode.Impulse); 
+        _targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse); 
+        transform.position = RandomSpawnPos();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(_minSpeed, _maxSpeed);
+    }
+    float RandomTorque()
+    {
+        return Random.Range(-_maxTorque, _maxTorque);
+    }
+    Vector3 RandomSpawnPos()
+    { 
+        return new Vector3(Random.Range(-_xRange, _xRange), _ySpawnPos); 
     }
 }
+
